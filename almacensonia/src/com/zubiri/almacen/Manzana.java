a54712883eeec47b4eb5b0a650b95ebaf5ca1116
egoitz <ebarrenalo12is@ikzubirimanteo.com>
2015-02-26 09:32:58 +0100

    almacen

---
+++ almacensonia/src/com/zubiri/almacen/Manzana.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.zubiri.almacen;
+
+import java.util.Scanner;
+
+/**
+ * @author xoni
+ *
+ */
+public class Manzana extends Producto  implements Enviable {
+
+	private String tipo;
+	private String color;
+	/**
+	 * 
+	 */
+	public Manzana(Distribuidor distribuidor, String marca, Double precio, String procedencia, String tipo, String color) {
+
+		super(distribuidor,marca,precio,procedencia);
+		this.tipo = tipo;
+		this.color = color;
+	}
+	
+	public Manzana(Scanner sc) throws Exception {
+
+		super(sc);
+		System.out.println("Tipo de manzana (DELICIOUS-GOLDEN-RED):");
+		this.setTipo(sc.next());
+	    System.out.println("Color de la manzana:");
+	    this.setColor(sc.next());
+		
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String tipo) throws Exception {
+		switch (tipo) {
+			
+		case "delicious":
+		case "DELICIOUS":
+		case "golden":
+		case "GOLDEN":
+		case "red":
+		case "RED":
+			this.tipo = tipo;
+			break;
+		default:
+			throw new Exception ("Tipo de manzana permitida: DELICIOUS,GOLDEN,RED");
+		}
+		
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public boolean esFragil() {
+    	return false;
+    }
+	
+	@Override
+	public void mostrarProducto() {
+		
+		System.out.println("MANZANA:");
+		super.mostrarProducto();
+		System.out.println("\ttipo: " + this.tipo);
+		System.out.println("\tcolor: " + this.color);
+}
+
+}
---
+++ almacensonia/src/com/zubiri/almacen/Productos.java
@@ -0,0 +1,62 @@
+package com.zubiri.almacen;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Productos {
+	
+	private ArrayList<Producto> listaProductos = new ArrayList<Producto>();	
+	
+	public Productos(Scanner sc) throws Exception {
+		int seleccion = -1;
+		do {
+			
+		try {
+		System.out.println("Cuantos productos quieres insertar? ");
+		seleccion = sc.nextInt();
+		} catch (InputMismatchException exception) {
+			System.out.println("Mete un numero");
+			sc.nextLine();
+		}
+	}while (seleccion < 0);
+		for (int i = 0; i < seleccion; i++) {
+			
+			System.out.println("�Qu� producto quieres insertar?");
+			System.out.println("MANZANA:--------1");
+			System.out.println("LECHE:----------2");
+			System.out.println("LECHUGA:--------3");
+		    
+			switch(sc.nextInt()) {
+				case 1:
+					Manzana manzana = new Manzana(sc);
+					listaProductos.add(manzana);
+					break;
+				case 2:
+					Leche leche = new Leche(sc);
+					listaProductos.add(leche);
+					break;
+				case 3:
+					Lechuga lechuga = new Lechuga(sc);
+					listaProductos.add(lechuga);
+					break;
+				default:
+					System.out.println("No ha insertado la opci�n correcta.");
+					break;
+			}			
+		}
+		
+	} 
+		 
+	public void mostrarProductos() {
+		
+		if (listaProductos.size() == 0) {
+			System.out.println("No se han cargado los productos");
+		}
+		for (int i = 0;i < listaProductos.size();i++) {
+			Producto producto = listaProductos.get(i);
+			producto.mostrarProducto(); //dispatching din�mico a partir de polimorfismo
+		}
+	}
+
+}
